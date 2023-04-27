b05e29396a0b3c55ea02b8b14faed2a6d5032712
Busra Ciftlik <busraciftlik96@gmail.com>
2023-04-27 13:34:16 +0300

    modified enums

--- src/com/busraciftlik/contacts/enums/Type.java
+++ src/com/busraciftlik/contacts/enums/Type.java
@@ -3,7 +3,7 @@ package com.busraciftlik.contacts.enums;
 import java.io.Serializable;
 
 public enum Type implements Serializable {
-    HOME, MOBILE, WORK
+    HOME, MOBILE, WORK;
 
     public static Type getType(String typeString) {
         for (Type type : Type.values()) {
